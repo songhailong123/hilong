bc8a841b3632f1d14edc7caaf3ab3e159aa5cfbc
shl <1370123868@qq.com>
2020-03-02 20:50:43 +0800

    我草泥马不给我保存？

--- hilong/src/views/Home.vue
+++ hilong/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+@Component({})
+export default class demo extends Vue {
+}
 </script>
+<style lang="scss" scoped>
+
+</style>
